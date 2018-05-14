3dbac6d83b83ca0f3df581c36405bb3440a7db12
dsdunn <dsdunnmusic@gmail.com>
2018-05-14 14:45:44 -0600

    Add boilerplate

--- test/utilities.js
+++
@@ -1,9 +0,0 @@
-const generateArray = (length) => {
-  let array = [];
-    for (let i = 0; i < length; i++) {
-      array.push(Math.floor(Math.random() * 100));
-  }
-  return array;
-}
-
-module.exports = generateArray;

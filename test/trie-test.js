7a2f29a6f58e9e97a75bbb684c6c819deae2a52e
dsdunn <dsdunnmusic@gmail.com>
2018-05-19 11:22:28 -0600

    Update tests and refactor suggest

--- test/trie-test.js
+++ test/trie-test.js
@@ -6,38 +6,68 @@ import fs from 'fs';
 
 
 describe('Trie', () => {
+
   let trie;
 
   beforeEach(() => {
     trie = new Trie();  
   })
 
+  describe('find', () => {
+
+    it('should exist', () => {
+      expect(trie.find).to.exist;
+    })
+
+    it('should return the last node of selected word', () => {
+      
+      trie.insert('cat');
+      trie.find('cat');
+
+      let actual = trie.root.child.c.child.a.child.t;
+
+      expect(trie.find('cat')).to.deep.equal(actual);
+    })
+  })
+
   describe('insert', () => {
 
+    it('should exist', () => {
+      expect(trie.insert).to.exist;
+    })
+
     it('should count words added', () => {
+
       expect(trie.numberOfWords).to.eq(0);
+      
       trie.insert('cat');
       trie.insert('cats');
       trie.insert('cottage');
       trie.insert('comb');
+
       expect(trie.numberOfWords).to.eq(4);
     });
 
     it('should not take the same word more than once', () => {
+      
       expect(trie.numberOfWords).to.eq(0);
+      
       trie.insert('cat');
       trie.insert('cat');
+
       expect(trie.numberOfWords).to.eq(1);
     })
 
     it('should change all strings to lowercase', () => {
+      
       trie.insert('DOG');
       trie.insert('dog');
+
       expect(trie.numberOfWords).to.eq(1);
     })
 
     it('should split words into nested nodes of letters', () => {
-
+      
       trie.insert('dog');
       
       let expected = 'g';
@@ -47,12 +77,13 @@ describe('Trie', () => {
     })
 
     it('should not dublicate nodes', () => {
+      
       trie.insert('dog');
       trie.insert('doggie');
       trie.insert('dad');
 
       let actual = Object.keys(trie.root.child.d.child.o.child).length;
-         
+
       expect(actual).to.eq(1);
     })
 
@@ -60,7 +91,12 @@ describe('Trie', () => {
 
   describe('suggest', () => {
 
+    it('should exist', () => {
+      expect(trie.suggest).to.exist;
+    })
+
     it('should return an empy array if argument absent, or invalid. or no suggestions exist', () => {
+      
       trie.insert('cat');
       trie.insert('cats');
       trie.insert('cottage');
@@ -73,6 +109,7 @@ describe('Trie', () => {
     })
 
     it('should return an empy array if no suggestions exist', () => {
+      
       trie.insert('cat');
       trie.insert('cats');
       trie.insert('cottage');
@@ -82,6 +119,7 @@ describe('Trie', () => {
     })
 
     it('should give suggestions if they exist', () => {
+      
       trie.insert('cat');
       trie.insert('cats');
       trie.insert('cottage');
@@ -90,7 +128,21 @@ describe('Trie', () => {
       expect(trie.suggest('co')).to.deep.eq([ 'cottage', 'comb' ]);
     });
 
+    it('should sort suggestions by popularity', () => {
+      
+      trie.insert('cats');
+      trie.insert('cottage');
+      trie.insert('comb');
+      trie.select('cottage');
+      trie.select('comb');
+      trie.select('comb');
+
+      expect(trie.suggest('c')).to.deep.equal(['comb','cottage','cats'])
+
+    })
+
     it('should be case insensitive', () => {
+      
       trie.insert('DOG');
       trie.insert('dog');
 
@@ -102,46 +154,68 @@ describe('Trie', () => {
 
   describe ('count', () => {
 
+    it('should exist', () => {
+      expect(trie.count).to.exist;
+    })
+
     it('should return the number of words in the trie', () => {
+      
       trie.insert('mouse');
+
       expect(trie.count()).to.eq(1);   
     })
   })
 
   describe('populate', () => {
+
+    it('should exist', () => {
+      expect(trie.populate).to.exist;
+    })
+
     it('should take an array of words and insert each word', () => {
+
+      const words = ['mouse', 'cat', 'dog', 'hamster'];
+
+      trie.populate(words);
+
+      expect(trie.count()).to.eq(4);
+
       const text = "/usr/share/dict/words"
       const dictionary = fs.readFileSync(text).toString().trim().split('\n')
 
-      const completion = new Trie();
+      trie.populate(dictionary);
 
-      completion.populate(dictionary);
-      expect(completion.count()).to.eq(234371);
-      expect(completion.suggest('world')).to.deep.eq(['world',
-  'worlded',
-  'worldful',
-  'worldish',
-  'worldless',
-  'worldlet',
-  'worldlike',
-  'worldlily',
-  'worldliness',
-  'worldling',
-  'worldly',
-  'worldmaker',
-  'worldmaking',
-  'worldproof',
-  'worldquake',
-  'worldward',
-  'worldwards',
-  'worldway',
-  'worldy']);
+      expect(trie.count()).to.eq(234371);
+      expect(trie.suggest('world')).to.deep.eq(
+        ['worldling',
+        'world',
+        'worldful',
+        'worldish',
+        'worldless',
+        'worldlet',
+        'worldlike',
+        'worldlily',
+        'worldliness',
+        'worlded',
+        'worldly',
+        'worldmaker',
+        'worldmaking',
+        'worldproof',
+        'worldquake',
+        'worldward',
+        'worldwards',
+        'worldway',
+        'worldy']);
     })
   })
 
   describe('delete', () => {
+    it('should exist', () => {
 
-    it('removes the nodes of input word that arent shared', () => {
+      expect(trie.delete).to.exist;
+    })
+
+    it('reasigns completedWord of last node to null, omitting it from future suggestions', () => {
 
       trie.insert('doggie');
       trie.insert('dog');
@@ -152,5 +226,62 @@ describe('Trie', () => {
     })
 
   })
+
+  describe('select', () => {
+
+    it('should exist', () => {
+      expect(trie.select).to.exist;
+    })
+
+    it('should increment the popularity of the selected word', () => {
+
+      trie.insert('dog');
+      trie.insert('cat');
+
+      expect(trie.find('dog').popularity).to.eq(0);
+
+      trie.select('dog');
+
+      expect(trie.find('dog').popularity).to.eq(1);
+      expect(trie.find('cat').popularity).to.eq(0);
+
+      trie.select('cat');
+      trie.select('cat');
+
+      expect(trie.find('cat').popularity).to.eq(2);
+    })
+  })
+
+  describe('includes', () => {
+
+    it('should exist', () => {
+      expect(trie.includes).to.exist;
+    })
+
+    it('should return true or false, depending on whether entered word exists in trie', () => {
+
+      trie.insert('dog');
+
+      expect(trie.includes('dog')).to.eq(true);
+      expect(trie.includes('cat')).to.eq(false);
+    })
+  })
+
+  describe('sortAndReduceSuggestions', () => {
+
+    it('should exist', () => {
+      expect(trie.sortAndReduceSuggestions).to.exist;
+    })
+
+    it('should take in an array of objects, and return an array of words sorted by popularity', () => {
+
+      let objArray = [
+      {word: 'dog', popularity: 0},
+      {word: 'cat', popularity: 2}];
+
+      let expectedArray = ['cat','dog'];
+
+      expect(trie.sortAndReduceSuggestions(objArray)).to.deep.equal(expectedArray);
+    })
+  })
 })
-      //console.log(JSON.stringify(trie, null, 4))
